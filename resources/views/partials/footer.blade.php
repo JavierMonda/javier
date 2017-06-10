7f30ccce337dab87ac692926c47eecffae37e673
JavierMonda <javier.raidpc@gmail.com>
2017-06-10 12:31:53 +0100

    Tag footer quitada

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,42 +1,40 @@
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <ul class="list-inline">
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li class="footer-menu-divider">&sdot;</li>
-                    <li>
-                        <a href="#sobremi">Sobre Mi</a>
-                    </li>
-                    <li class="footer-menu-divider">&sdot;</li>
-                    <li>
-                        <a href="#portfolio">Portfolio</a>
-                    </li>
-                    <li class="footer-menu-divider">&sdot;</li>
-                    <li>
-                        <a href="{{ url('/blog/contacto/') }}">Contacto</a>
-                    </li>
-                    <li class="footer-menu-divider">&sdot;</li>
-                    <li>
-                        <a href="{{ url('/blog/aviso-legal/') }}">Aviso Legal</a>
-                    </li>
-                    <li class="footer-menu-divider">&sdot;</li>
-                    <li>
-                        <a href="{{ url('/blog/politica-de-cookies/') }}">Política de Cookies</a>
-                    </li>
-                </ul>
-                
-            </div>
-            <div class="col-md-4">
-                <div itemscope itemtype="http://schema.org/Person">
-                    Nombre: <span itemprop="name">Javier Jiménez García</span>
-                    Foto: <img src="/assets/bootstrap/img/perfil.png" itemprop="image" style="height: 32px; width: 32px;" alt="Foto de Javier Jiménez García"/>
-                    Web: <a href="https://javierjg.es" itemprop="url">javierjg.es</a>
-                </div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <ul class="list-inline">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li class="footer-menu-divider">&sdot;</li>
+                <li>
+                    <a href="#sobremi">Sobre Mi</a>
+                </li>
+                <li class="footer-menu-divider">&sdot;</li>
+                <li>
+                    <a href="#portfolio">Portfolio</a>
+                </li>
+                <li class="footer-menu-divider">&sdot;</li>
+                <li>
+                    <a href="{{ url('/blog/contacto/') }}">Contacto</a>
+                </li>
+                <li class="footer-menu-divider">&sdot;</li>
+                <li>
+                    <a href="{{ url('/blog/aviso-legal/') }}">Aviso Legal</a>
+                </li>
+                <li class="footer-menu-divider">&sdot;</li>
+                <li>
+                    <a href="{{ url('/blog/politica-de-cookies/') }}">Política de Cookies</a>
+                </li>
+            </ul>
+            
+        </div>
+        <div class="col-md-4">
+            <div itemscope itemtype="http://schema.org/Person">
+                Nombre: <span itemprop="name">Javier Jiménez García</span>
+                Foto: <img src="/assets/bootstrap/img/perfil.png" itemprop="image" style="height: 32px; width: 32px;" alt="Foto de Javier Jiménez García"/>
+                Web: <a href="https://javierjg.es" itemprop="url">javierjg.es</a>
             </div>
         </div>
-        <p class="copyright text-muted small">Javier Jiménez - Diseño Web</p>       
     </div>
-</footer>
+    <p class="copyright text-muted small">Javier Jiménez - Diseño Web</p>       
+</div>
