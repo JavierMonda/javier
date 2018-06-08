1ca34233d28fbec03b057a971c510b77aef8737b
javiermonda <javier.raidpc@gmail.com>
2018-06-08 05:44:18 +0100

    añadido consentimiento

---
+++ resources/views/consentimiento.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="container aboutMe">
+    <div class="row">
+        <div class="col-lg-12">
+           <script id="CookieDeclaration" src="https://consent.cookiebot.com/648a5815-40ff-4bcc-b70b-f39cfc07bc06/cd.js" type="text/javascript" async></script>
+        </div>
+    </div>   
+</div>
+
+@stop
